4672f54cd53c48144163e181ecf51b4e81c74e20
Justin <jstn.anthony1@gmail.com>
2021-08-09 17:35:01 -0400

    Adding project files

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.scss'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello!</h1>
+  <a href="QA/qa.html">QA Page</a>
+  <a href="Security/security.html">Security</a>
+  <a href="BadExample/badExample.html">Bad Example</a>
+`
